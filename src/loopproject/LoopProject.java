59a1b7f19fbd839152be2e82ba24db5006474719
mavi <mavikanamoutandou@gmail.com>
2022-11-02 13:28:03 -0700

    javaCore

---
+++ src/loopproject/LoopProject.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package loopproject;
+
+/**
+ *
+ * @author user
+ */
+public class LoopProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        int s = 0;
+        int i = 1;
+        while (1000 > i) {
+            s = s + i;
+            i++;
+        }
+        
+        System.out.println("la somme est : " + s);
+
+    }
+
+}
